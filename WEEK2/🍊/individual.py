26d9f79461db6b6aa04c77ec5765bf1923a8d6cc
dydhslxl7 <dydhslxl7@naver.com>
2022-08-14 16:49:59 +0900

    1072_upload

--- WEEK2/🍊/individual.py
+++ WEEK2/🍊/individual.py
@@ -1,11 +1,12 @@
 # 1071_20220811
+'''
 num = input()
 len = len(num)
 flag = '-1'
 
 while flag != '0':
     flag = num.find(' ') + 1
-    n1 = num[0:flag].strip()
+    n1 = num[0:flag].strip() # 문자열 자르기
     num = num[flag:len]
     if n1 == '0':
         flag = '0'
@@ -14,3 +15,10 @@ while flag != '0':
             print(n1)
         else:
             print(num)
+'''
+
+# 1072_20220814
+cnt = int(input());
+nums = input().split();
+for i in range(cnt):
+    print(nums[i]);
