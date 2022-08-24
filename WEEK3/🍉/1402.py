db0defffef1c74665e26835f8ee3b7bc26fef30c
suugit <gsuh021@gmail.com>
2022-08-24 19:57:23 +0900

    수정

--- WEEK3/🍉/1402.py
+++ WEEK3/🍉/1402.py
@@ -1,6 +1,9 @@
-k = int(input()) -1
+k = int(input())
 arr = input().split()[:k]
+
 
 while(len(arr)):
    print(arr.pop(),end=' ')
-
+   
+#print(' '.join(arr[::-1]))
+#print(' '.join(reversed(arr)))
