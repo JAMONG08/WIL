b76e015699b45c5766a1b8d31495506ec6652c1a
dydhslxl7 <dydhslxl7@naver.com>
2022-08-28 21:03:38 +0900

    팩토리얼

--- WEEK4/🍊/Q_1912.py
+++ WEEK4/🍊/Q_1912.py
@@ -1,1 +1,13 @@
 ### 1912 : (재귀함수) 팩토리얼 계산
+num = int(input())
+sum = 1
+
+def factorial(num):
+    if(num > 1):
+        global sum
+        sum *= num
+        factorial(num-1)
+    else:
+        print(sum)
+
+factorial(num)
