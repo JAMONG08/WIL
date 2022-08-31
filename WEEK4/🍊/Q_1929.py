17855b60e68ee85d6c9581fba191cc36a93d07a0
dydhslxl7 <dydhslxl7@naver.com>
2022-08-31 20:50:14 +0900

    스택에 관한 수정

--- WEEK4/🍊/Q_1929.py
+++ WEEK4/🍊/Q_1929.py
@@ -3,17 +3,14 @@ num = int(input())
 stack = []
 
 def reverse(num):
-    if(num != 1):
-        stack.append(num)
+    stack.append(num)
 
+    if(num != 1):
         if(num % 2 != 0):
             num = 3 * num + 1
         else:
             num //= 2
         reverse(num)
-
-    else:
-        print(1)
 
     print(stack.pop())
 
@@ -21,7 +18,15 @@ reverse(num)
 
 '''
     스택과 재귀함수의 콜라보
+
     맨 마지막 1을 표시하기 위해 else문에 print(1)
     7번 문제에서 걸림 -> if문 1 검사 추가
+
+    -> 수정
+    reverse 함수 호출되는 횟수만큼 스택의 값이 없어서
+    에러뜨기 때문에 함수 호출하면 무조건 스택에 값을
+    넣는 방식으로 변경
+
     // : 정수만 구하는 나눗셈
+    / : 소수까지 구하는 나눗셈
 '''
