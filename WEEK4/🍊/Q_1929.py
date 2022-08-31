06869a4ccfcbebd6bfc2d009155ae3b15895ddc3
dydhslxl7 <dydhslxl7@naver.com>
2022-08-31 20:07:35 +0900

    주석 추가

--- WEEK4/🍊/Q_1929.py
+++ WEEK4/🍊/Q_1929.py
@@ -10,8 +10,8 @@ def reverse(num):
             num = 3 * num + 1
         else:
             num //= 2
-        
         reverse(num)
+
     else:
         print(1)
 
@@ -23,4 +23,5 @@ reverse(num)
     스택과 재귀함수의 콜라보
     맨 마지막 1을 표시하기 위해 else문에 print(1)
     7번 문제에서 걸림 -> if문 1 검사 추가
+    // : 정수만 구하는 나눗셈
 '''
