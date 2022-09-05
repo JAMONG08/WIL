dbcef047d95d03cb66dc287263fc1a9d32bec300
dydhslxl7 <dydhslxl7@naver.com>
2022-09-05 21:29:56 +0900

    간단 이론정리, 최대공약수

--- WEEK5/🍊/Q_2623.py
+++ WEEK5/🍊/Q_2623.py
@@ -1,1 +1,23 @@
-### 2641 : 숏다리의 계단 오르기 (Small)
+### 2641 : 최대공약수 구하기
+'''
+    최대공약수 수학 : https://mathbang.net/202
+
+    1. 그냥 둘 한꺼번에 나눠서 구함 - https://mathbang.net/202
+'''
+
+nums = [int(str) for str in input().split()]
+if(nums[0] <= nums[1]):
+    end = nums[0]
+else:
+    end = nums[1]
+
+min = 0
+for i in range(1, end+1):
+    if(nums[0] % i == 0 and nums[1] % i == 0):
+        min = i
+
+print(min)
+'''
+    리스트의 모든 문자열을 int 값으로 변경 : 
+        https://hbesthee.tistory.com/1727
+'''
