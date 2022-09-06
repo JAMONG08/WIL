f72ce8eefd1f1b92819c8e9d0ce57c21c09be359
dydhslxl7 <dydhslxl7@naver.com>
2022-09-06 21:35:17 +0900

    이론 정리, 최대공약수 지수

--- WEEK5/🍊/Q_2623.py
+++ WEEK5/🍊/Q_2623.py
@@ -21,3 +21,46 @@ print(min)
     리스트의 모든 문자열을 int 값으로 변경 : 
         https://hbesthee.tistory.com/1727
 '''
+
+
+
+'''
+    2. 각각의 약수를 구함 - http://www.tcpschool.com/codingmath/common
+'''
+cd = {}
+cd[2] = 0
+total = 1
+
+def GCD(n, end=0):
+    global total
+    key = 2
+    while True:
+        if(n < key):
+            return
+        
+        if((key == 2 or key % 2 != 0) and n % key == 0):
+            if(end == 0):
+                cd[key] += 1
+            else:
+                if(cd[key] > 0):
+                    cd[key] -= 1
+                    total *= key
+            n //= key
+        else:
+            key += 1
+            if not key in cd:
+                cd[key] = 0
+
+
+nums = [int(str) for str in input().split()]
+if(nums[0] <= nums[1]):
+    GCD(nums[1])
+    GCD(nums[0], 1)
+else:
+    GCD(nums[0])
+    GCD(nums[1], 1)
+
+print(total)
+'''
+    동적변수 : locals()['cd{}'.format(k)] = {}
+'''
--- WEEK5/🍊/Search.md
+++ WEEK5/🍊/Search.md
@@ -22,3 +22,12 @@
 3. Dictionary : { }
 
 [참조](https://hashcode.co.kr/questions/4118/%EC%9D%98-%EC%B0%A8%EC%9D%B4%EC%99%80-%EC%82%AC%EC%9A%A9%ED%95%B4%EC%95%BC-%ED%95%A0-%EA%B3%B3)
+
+
+- - -
+<br>
+
+# 백트래킹
+> 모든 경우의 수를 전부 고려하는 알고리즘 상태공간을 트리로 나타낼 수 있을 때 적합한 방식이다. 일종의 트리 탐색 알고리즘이라고 봐도 된다. 방식에 따라서 깊이우선탐색(Depth First Search. DFS)과 너비우선탐색(Breadth First Search. BFS). 모든 경우의 수를 고려해야 하는 문제라면, DFS가 낫다. BFS로도 구현이 물론 가능하지만, BFS로 구현했다간 큐의 크기가 커진다. 심지어 속도도 똑같다. 따라서 경우의 수 구하기는 일반적으로 DFS가 편리하다. 대다수의 문제들은 DFS를 써도 일단 답은 나온다.
+
+[참조](https://fomaios.tistory.com/entry/Algorithm-%EB%B0%B1%ED%8A%B8%EB%9E%98%ED%82%B9Backtracking%EC%9D%B4%EB%9E%80)
