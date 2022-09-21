8cd3d25405d3465571ccb3afa34140b6e57a6916
suugit <gsuh021@gmail.com>
2022-09-21 20:56:15 +0900

    week7 추가

--- WEEK5/🍉/2623.py
+++ WEEK5/🍉/2623.py
@@ -1,7 +1,8 @@
 #두 정수 a , b 를 입력받아서, a , b 의 최대공약수를 출력하시오.
 #GCD, Greateast Common Division
 #14 o(n)
-a, b = map(int, input().split())
+# 12456 3456
+# a, b = map(int, input().split())
 
 # def gcd1(a, b):
 #     for i in range(min(a,b), 0, -1):
@@ -11,12 +12,12 @@ a, b = map(int, input().split())
 
 # gcd1(a, b)
 
-# 유클리드 호제
-a, b = map(int, input().split())
-def gcd2(a, b):
-    return b if a == 0 else gcd2(b%a, a);
+# 유클리드 호제법 - 수학 정수론 ~~
+# a, b = map(int, input().split())
+# def gcd2(a, b):
+#     return a if a == 0 else gcd2(b%a, a);
 
-print(gcd2(a,b))
+# print(gcd2(a,b))
 
 #16 내장함수
 # import math
