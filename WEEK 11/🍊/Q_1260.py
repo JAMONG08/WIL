5a6c3e2d0a195c2877053899f7957afb544ba56c
dydhslxl7 <dydhslxl7@naver.com>
2022-10-31 22:53:18 +0900

    1260 BFS

--- WEEK 11/🍊/Q_1260.py
+++ WEEK 11/🍊/Q_1260.py
@@ -11,12 +11,27 @@ def DFS(s):
         if arr[s][i] == 1 and i not in stack:
             DFS(i)
 
-# def BFS():
+
+
+def BFS(s, visited=[]):
+    global arr, stack
+
+    stack.append(s)
+
+    while stack:
+        s = stack.popleft()
+        print(s, end=' ')
+        visited.append(s)
+
+        for i in range(len(arr[s])):
+            if arr[s][i] == 1 and i not in visited and i not in stack:
+                stack.append(i)
+
 
 
 n, m, s = map(int, input().split())
 arr = [[0 for j in range(n+1)] for i in range(n+1)]
-stack = deque[]
+stack = deque()
 
 for i in range(0, m):
     t1, t2 = map(int, input().split())
@@ -27,9 +42,12 @@ for i in range(0, m):
 
 DFS(s)
 print(*stack)
+stack.clear()
+BFS(s)
 
 
 '''
 https://jokerldg.github.io/algorithm/2021/03/22/dfs-bfs.html
 https://0422.tistory.com/43
+https://heytech.tistory.com/56
 '''
