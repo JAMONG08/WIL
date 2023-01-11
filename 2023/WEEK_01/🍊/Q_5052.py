db18776b534ad5b8f4efb07923d0f5a33c7e1b44
dydhslxl7 <dydhslxl7@naver.com>
2023-01-11 20:30:05 +0900

    5052 update

--- 2023/WEEK_01/🍊/Q_5052.py
+++ 2023/WEEK_01/🍊/Q_5052.py
@@ -1,27 +1,21 @@
 # 전화번호 목록
 import sys
-N1 = int(sys.stdin.readline())
-# A = []
+N1 = int(input())
+A = []
 
 for i in range(0, N1):
-    N2 = int(sys.stdin.readline())
-    ph1 = list(map(str, sys.stdin.readline()))
+    N2 = int(input())
+    ph1 = list(map(str, input()))
     l = len(ph1)
-    # A.append('YES')
-    flag = True
+    A.append('YES')
 
     for j in range(1, N2):
-        ph2 = list(map(str, sys.stdin.readline()))
+        ph2 = list(map(str, input()))
         if ph1 == ph2[:l]:
-            # A[i] = 'NO'
-            flag = False
-            print('NO')
+            A[i] = 'NO'
             break
-    
-    if(flag):
-        print('YES')
 
-# print(*A, sep='\n')
+print(*A, sep='\n')
 
 '''
 https://velog.io/@injoon2019/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-5052-%EC%A0%84%ED%99%94%EB%B2%88%ED%98%B8-%EB%AA%A9%EB%A1%9D-%ED%8C%8C%EC%9D%B4%EC%8D%AC
